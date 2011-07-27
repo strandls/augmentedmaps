95695025c3f1c2d70d80bcc500aeea4d791eaae7
ravurihareesh <hareesh@strandls.com>
2011-07-27 10:54:35 +0530

    checking in the java script added to resize the mail text field according to the size of the table in the left

---
+++ src/drupal_custom/modules/indiatreepix/scripts/indiatreepix.js
@@ -0,0 +1,12 @@
+if(Drupal.jsEnabled) {
+  $(document).ready(function()  {
+
+//Modify the height of mail text, according to the height of the table in the left
+$('div.field-field-itp-mail-text').css('height', $('table.itp_table').height());
+
+
+  });
+}
+
+
+
